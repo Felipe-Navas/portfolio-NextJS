31655973cdbc80dc0b0e950cac8402bbef1537c6
Felipe Navas <felipenavas.itec@gmail.com>
2024-08-14 08:31:02 -0300

    Added MagicButton

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,3 +1,5 @@
+import { FaLocationArrow } from 'react-icons/fa6'
+import { MagicButton } from './ui/MagicButton'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
@@ -27,11 +29,19 @@ export const Main = () => {
 
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming concepts into seamless experiences"
+            words="Transforming Concepts into Seamless User Experiences"
           />
-          <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Felipe, a software engineer from Argentina
           </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
     </div>
